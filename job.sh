f71f1d3c31b1a0c648971aec33863ff446f55579
Zhao Wang <68866053+wangzhaomxy@users.noreply.github.com>
2026-08-07 12:40:59 +1000

    update path

--- job.sh
+++ job.sh
@@ -4,8 +4,8 @@
 #SBATCH --gres=gpu:1
 ##SBATCH --mem=16G
 #SBATCH --time=00:05:00
-#SBATCH --output=cuda_check_%j.out
-#SBATCH --error=cuda_check_%j.err
+#SBATCH --output=/home/Student/s4670484/comp3710_2026_lab1/result/cuda_check_%j.out
+#SBATCH --error=/home/Student/s4670484/comp3710_2026_lab1/result/cuda_check_%j.err
 
 # Activate your conda environment
 source $HOME/miniconda3/bin/activate

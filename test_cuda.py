c887a1ed104a557181d9a1a3e5869fa019965bb4
Zhao Wang <68866053+wangzhaomxy@users.noreply.github.com>
2026-08-07 12:36:41 +1000

    update new

--- test_cuda.py
+++ test_cuda.py
@@ -3,3 +3,4 @@ print("CUDA available:", torch.cuda.is_available())
 if torch.cuda.is_available():
     print("Device count :", torch.cuda.device_count())
     print("Device name :", torch.cuda.get_device_name(0))
+    print("New update")
